16f7e0145d58286f6c2aacbd4235a2873ec78f18
douglas_crawford <doug.a.crawford@gmail.com>
2020-05-29 16:04:35 -0400

    Removed helpers for controllers generated using -g controller

---
+++ frontend/cap_app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDom.render(<h1>It works</h1>, root)
+})
