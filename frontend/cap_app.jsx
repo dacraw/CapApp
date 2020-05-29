4a77483b5205f1b8d31805872c0f3eb70d643cae
douglas_crawford <doug.a.crawford@gmail.com>
2020-05-29 16:35:53 -0400

    Fixed problem with jquery configuration; now apiutil ajax requests can add users to database from window console

--- frontend/cap_app.jsx
+++ frontend/cap_app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import * as APIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.APIUtil = APIUtil;
+
     const root = document.getElementById('root');
     ReactDOM.render(<h1>It works</h1>, root)
 })
