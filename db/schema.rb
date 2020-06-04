f0f2a559d3bbe233ed2f637da4bd5e14839fc755
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-04 09:48:39 -0400

    Created portfolios table. added seed for new demo user to have cash available.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_154930) do
+ActiveRecord::Schema.define(version: 2020_06_04_133926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "portfolios", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.float "cash_available", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_portfolios_on_user_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
