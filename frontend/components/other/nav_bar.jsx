453fefa2b7235c6381fe0ff3477caed297e86266
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-04 12:01:40 -0400

    merged updated nav bar for splash page from PSTinvestments

--- frontend/components/other/nav_bar.jsx
+++ frontend/components/other/nav_bar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import DashNavBar from './dash_nav_bar';
 
 class NavBar extends React.Component {
     constructor(props){
@@ -8,47 +9,40 @@ class NavBar extends React.Component {
     }
 
     toggleDropdown(e){
+        // debugger;
         e.preventDefault();
         document.querySelector('.header-products-dropdown').classList.toggle('show');
     }
 
+
+
     render() {
         const { currentUser, logout } = this.props;
-        if (currentUser) { 
-            return (
-                <nav className="header-nav">
-                        <h2>Logged In</h2>
-                        <p>{currentUser.username}</p>
-                        <button onClick={logout}>logout</button>
-                </nav>
-            )
-        } else { 
-            return (
-                <div className="header-container">
-                    <section className="header">
-                        <img className="logo" src={window.logo} />
-                        <nav className="splash-header-nav">
-                            <li className="header-products">
-                                <a onClick={this.toggleDropdown}>Products &#9660;</a>
-                            </li>
-                            <li><Link to='/learn'>Learn</Link></li>
-                            <li><Link to='/support'>Support</Link></li>
-                        </nav>
-                        <div className="header-logged-options">
-                            <Link to="/login">Sign In</Link>
-                            <Link to="signup">Sign Up</Link>
-                        </div>
-                    </section>
-                    <ul className="header-products-dropdown">
-                                    <li><Link to='/stocks-funds'>Stocks & Funds</Link></li>
-                                    <li><Link to='/options'>Options</Link></li>
-                                    <li><Link to='/gold'>Gold</Link></li>
-                                    <li><Link to='/cash-management'>Cash Management</Link></li>
-                                    <li><Link to='/crypto'>Crypto</Link></li>
-                    </ul>
-                </div>
-            )
-        }
+        return (
+            <div className="header-container">
+                <section className="header">
+                    <img className="logo" src={window.logo} />
+                    <nav className="splash-header-nav">
+                        <li className="header-products">
+                            <a onClick={this.toggleDropdown}>Products &#9660;</a>
+                        </li>
+                        <li><Link to='/learn'>Learn</Link></li>
+                        <li><Link to='/support'>Support</Link></li>
+                    </nav>
+                    <div className="header-logged-options">
+                        <Link to="/login">Sign In</Link>
+                        <Link to="signup">Sign Up</Link>
+                    </div>
+                </section>
+                <ul className="header-products-dropdown">
+                                <li><Link to='/stocks-funds'>Stocks & Funds</Link></li>
+                                <li><Link to='/options'>Options</Link></li>
+                                <li><Link to='/gold'>Gold</Link></li>
+                                <li><Link to='/cash-management'>Cash Management</Link></li>
+                                <li><Link to='/crypto'>Crypto</Link></li>
+                </ul>
+            </div>
+        )
     }
 };
 
