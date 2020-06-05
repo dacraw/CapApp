c84b82e7a30d45594df96f60f0b2d8f95cbf95d9
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-04 21:55:59 -0400

    created rough skeleton for sidebar in functional_component.

--- frontend/components/functional_component/functional_component.jsx
+++ frontend/components/functional_component/functional_component.jsx
@@ -11,10 +11,45 @@ class FunctionalComponent extends Component {
     
     render() {
         return (
-            <main className="functional-container">
-                <section className="financials-container">
-                    test
+            <main className="functional-component-container">
+                <section className="main">
+                    <p>test</p>
                 </section>
+                <ul className="side-nav">
+                    <li className="buy-sell">
+                        <a>Buy TSLA</a>
+                        <a>Sell TSLA</a>
+                    </li>
+                    <hr />
+                    <li>
+                        <form className="pst-form">
+                            <section className="line">
+                                <label>Invest In</label>
+                                <select>
+                                    <option selected>Shares</option>
+                                    <option>Dollars</option>
+                                </select>
+                            </section>
+                            <section className="line">
+                                <label>Shares</label>
+                                <input type="number" placeholder="0" />
+                            </section>
+                            <section className="line">
+                                <label>Market Price</label>
+                                <data>$12.99</data>
+                            </section>
+                            <section className="line">
+                                <label>Market Price</label>
+                                <data>$12.99</data>
+                            </section>
+                            <button>Review Order</button>
+                            <hr />
+                            <details className="buying-power">
+                                $10,000 buying power available.
+                            </details>
+                        </form>
+                    </li>
+                </ul>
             </main>
         )
     }
