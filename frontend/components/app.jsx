b08b4b956a952c6d8fcf9d7e51f3db3107cd6760
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-04 20:44:40 -0400

    fixed post restruct. file dir issues (i.e. imports)

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import LoginContainer from './session/login_container';
 import SignupContainer from './session/signup_container';
-import NavBarContainer from './other/nav_bar_container';
-import DashNavBarContainer from './other/dash_nav_bar_container';
+import NavBarContainer from './nav_bar/nav_bar_container';
+import DashNavBarContainer from './nav_bar/dash_nav_bar_container';
 import {AuthRoute} from '../util/route_util';
 import {ProtRoute} from '../util/route_util';
 import Dashboard from './dashboard/dash_main';
-import Splash from './other/splash';
+import Splash from './splash/splash';
 
 const App = (props) => {
     debugger
