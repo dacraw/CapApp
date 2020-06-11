557fe094befe2bca7a0fddc0f7aa26e39ff957a5
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-10 22:36:51 -0400

    Merging branches from backup

---
+++ frontend/components/dashboard/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    
+    render() {
+        return (
+            <>
+                <h1>placeholder</h1>
+            </>
+        )
+    }
+}
+export default Dashboard;
