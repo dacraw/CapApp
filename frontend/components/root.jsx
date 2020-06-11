9288b6ba75bf488155e9c47e98811f5813c734ae
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-10 21:27:48 -0400

    Merging with master

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import App from './app';
 import { HashRouter } from 'react-router-dom';
-import ScrollToTop from './util/scroll_to_top'
 
 const Root = ({ store }) => {
     return (
         <>
             <Provider store={store}>
                 <HashRouter>
-                    <ScrollToTop>
                         <App />
-                    </ScrollToTop>
                 </HashRouter>
             </Provider>
         </>
