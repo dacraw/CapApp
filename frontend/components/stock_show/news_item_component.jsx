7777b0a7c5468d61bf5217be77ffd6e9f64b3c08
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-29 10:46:12 -0400

    Style news

--- frontend/components/stock_show/news_item_component.jsx
+++ frontend/components/stock_show/news_item_component.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 export default ({article}) => {
     return (
         <li className="news-item">
-            <div>
-                <h2>{article.source.name}</h2>
-                <h3>{article.title}</h3>
-                <h4>{article.description.slice(0,20)}...</h4>
-            </div>
-            <img src={window.stockThree} />
+            <a target="_blank" href={article.url}>
+                <div>
+                    <h2>{article.source.name}</h2>
+                    <h3>{article.title}</h3>
+                    <h4>{article.description}</h4>
+                </div>
+                <img src={article.urlToImage} />
+            </a>
         </li>
     )
 };
