c299bd6a35a0bfe974a4ac71f876a47684a6d8da
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-01 16:32:21 -0400

    Cleanup flex styling

--- frontend/components/dashboard/DashMainSidebarGraph.jsx
+++ frontend/components/dashboard/DashMainSidebarGraph.jsx
@@ -1,25 +1,25 @@
 import React, {Component} from 'react'
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import {useSelector} from 'react-redux'
 
-class DashMainSidebarGraph extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            
-        }
-    }
-    
-    render() {
+const DashMainSidebarGraph = ({data, symbol}) => {
+    const percentageChange = useSelector(state => {
+        debugger
+        return state.entities.stocks[symbol].percentageChange;
+    });
+    const strokeColor = (percentageChange >= 0) ? "rgb(16, 197, 40)" : "#ff4f0b";
         return (
             <>
-                <LineChart onMouseMove={this.handleEnter} onMouseLeave={() => this.handleLeave(stock.price)} width={710} height={200} data={data}>
-                    <Line isAnimationActive={false} connectNulls={true} type="linear" dataKey={"average"} stroke={strokeColor} dot={false} strokeWidth="2" />
-                    <XAxis hide={true} dataKey="label" />
-                    <YAxis domain={['auto', 'auto']} hide={true} />
-                    <Tooltip wrapperStyle={{left: -30, fontSize: '.8em'}}isAnimationActive={false} filterNull={true} position={{y: -20}} content={<CustomTooltip />} payload={[{ name: "label", value: "average" }]} />
-                </LineChart>
+                <ResponsiveContainer>
+                    <LineChart data={data}>
+                        <Line isAnimationActive={false} connectNulls={true} type="linear" dataKey={"average"} stroke={strokeColor} dot={false} strokeWidth="2" />
+                        <XAxis hide={true} dataKey="label" />
+                        <YAxis domain={['auto', 'auto']} hide={true} />
+                    </LineChart>
+                </ResponsiveContainer>
             </>
         )
-    }
+
 }
 
 export default DashMainSidebarGraph;
--- frontend/components/dashboard/sidebar_portfolio_item.jsx
+++ frontend/components/dashboard/sidebar_portfolio_item.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import DashMainSidebarGraph from './DashMainSidebarGraph'
+import { useSelector } from 'react-redux'
+
 
 export default ({ownedStock, stocks}) => {
     if (!stocks) return null;
-
     const dollarChangeStyle = (stocks[ownedStock[1].symbol].percentageChange <= 0) ? "negative" : "";
-
+    const chart = useSelector(state => {
+        // debugger
+        state.entities.stocks[ownedStock[1].symbol].chart;
+    });
+    
+    // debugger
     return (
         <>
             <li className="owned-stocks">
@@ -16,7 +22,7 @@ export default ({ownedStock, stocks}) => {
                         <div className="num-shares">{ownedStock[1].num_shares}</div>
                     </div>
                     <div className="stock-mini-graph">
-                        <DashMainSidebarGraph />
+                        <DashMainSidebarGraph data={chart} symbol={ownedStock[1].symbol} />
                     </div>
                     <div className="stock-info">
                         <div className="price">{stocks[ownedStock[1].symbol].price}</div>
