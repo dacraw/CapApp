797dbd2b41a47cabee4a244e775e6b47fd7b8294
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-01 15:40:58 -0400

    Add negative styling for negative price changes

--- frontend/components/dashboard/sidebar_portfolio_item.jsx
+++ frontend/components/dashboard/sidebar_portfolio_item.jsx
@@ -4,17 +4,19 @@ import {Link} from 'react-router-dom'
 export default ({ownedStock, stocks}) => {
     if (!stocks) return null;
 
+    const dollarChangeStyle = (stocks[ownedStock[1].symbol].percentageChange <= 0) ? "negative" : "";
+
     return (
         <>
             <li className="owned-stocks">
-                <Link to={`/stocks/${ownedStock[1].symbol}`}> 
+                <Link className="stock-link" to={`/stocks/${ownedStock[1].symbol}`}> 
                     <div className="symbol-numshares">
                         <div className="symbol">{ownedStock[1].symbol}</div>
                         <div className="num-shares">{ownedStock[1].num_shares}</div>
                     </div>
                     <div className="stock-info">
                         <div className="price">{stocks[ownedStock[1].symbol].price}</div>
-                        <div className="percentage-change">{stocks[ownedStock[1].symbol].percentageChange}</div>
+                        <div className={`percentage-change ${dollarChangeStyle}`}>{stocks[ownedStock[1].symbol].percentageChange}%</div>
                     </div>
                 </Link>
             </li>
