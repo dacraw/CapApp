8ea0c283c4dfc1d85379ed4f0f6e1ec7857e6a78
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-01 21:30:30 -0400

    Style news

--- frontend/components/other/NewsComponentItem.jsx
+++ frontend/components/other/NewsComponentItem.jsx
@@ -6,10 +6,16 @@ export default ({newsItem}) => {
     return (
         <>
             <div className="dashboard-news-item">
-                <div className="row">
-                    <i className="fas fa-dollar-sign"></i>
-                    <p className="source">{newsItem.source.name}</p>
-                </div>
+                <a target="_blank" href={newsItem.url}>
+                    <div className="row">
+                        {/* <i className="fas fa-dollar-sign"></i> */}
+                        <p className="source">{newsItem.source.name}</p>
+                    </div>
+                    <div className="row">
+                        <h3 className="title">{newsItem.title}</h3>
+                        <img src={newsItem.urlToImage} />
+                    </div>
+                </a>
             </div>
         </>
     )
