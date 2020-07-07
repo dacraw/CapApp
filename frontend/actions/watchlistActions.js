fd02c52f4d6f86e9f0f9c9b0ba57ab2e656b2df5
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-07 17:57:05 -0400

    Create watchlist actions

---
+++ frontend/actions/watchlistActions.js
@@ -0,0 +1,38 @@
+import * as WatchlistUtil from '../util/watchlistUtil'
+
+export const RECEIVE_ALL_WATCHLISTS = "RECEIVE_ALL_WATCHLISTS";
+export const RECEIVE_SINGLE_WATCHLIST = "RECEIVE_SINGLE_WATCHLIST";
+export const RECEIVE_WATCHLIST_ERRORS = "RECEIVE_WATCHLIST_ERRORS";
+
+const receiveAllWatchLists = watchlists => ({
+    type: RECEIVE_ALL_WATCHLISTS,
+    watchlists,
+});
+
+const receiveSingleWatchlist = watchlist => ({
+    type: RECEIVE_SINGLE_WATCHLIST,
+    watchlist,
+});
+
+const receiveWatchlistErrors = errors => ({
+    type: RECEIVE_WATCHLIST_ERRORS,
+    errors
+});
+
+export const fetchAllWatchlists = () => dispatch => {
+    return WatchlistUtil.fetchAllWatchlists()
+        .then(
+            response => dispatch(receiveAllWatchLists(response)),
+            errors => dispatch(receiveWatchlistErrors(errors.json))
+        )
+}
+
+export const fetchSingleWatchlist = () => dispatch => {
+    return WatchlistUtil.fetchSingleWatchlist()
+        .then(
+            response => dispatch(receiveSingleWatchList(response)),
+            errors => dispatch(receiveWatchlistErrors(errors.json))
+        )
+}
+
+
