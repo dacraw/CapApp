32edd199cf2d29a02406b2566fb328aaf2e7a5a1
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-07 12:41:23 -0400

    Create combinedStats object to pass data into graph component

--- frontend/components/dashboard/main.jsx
+++ frontend/components/dashboard/main.jsx
@@ -11,28 +11,47 @@ class Dashboard extends React.Component {
         }
     }
     render() {
-        const { stocks, user: {ownedStocks} } = this.props;
+        const { stocks, user, user: {ownedStocks} } = this.props;
         debugger
 
-        if (!stocks || !ownedStocks) return null;
+        if (!stocks || !ownedStocks || !user) return null;
 
         const symbols = Object.keys(ownedStocks);
         
-        const combinedStats = {};
-        symbols.forEach( (symbol, idx) => {
-            stocks[symbol].chart.forEach( (dataPoint, chartIdx) => {
-                if (combinedStats[dataPoint.label] === undefined) combinedStats[dataPoint.label] = 0;
+        // this block combines the averages for all of the user's portfolios; 
+        // please note that for demo purposes, the data is pulled from a sample chart that does not use up-to-date info
 
-                let value = dataPoint.average;
-                if (value === null){
-                    value = stocks[symbol].chart[stocks[symbol].chart.length - 1].average;
+        // structure matching graphcomponent
+        let combinedStats = {};
+        combinedStats['chart'];
+        combinedStats['price'];
+        combinedStats['percentageChange'];
+        combinedStats['dollarChange'];
+        symbols.forEach( (symbol, idx) => {
+            if (idx == 0) {
+                combinedStats['chart'] = stocks[symbol].chart
+            }
+            stocks[symbol].chart.forEach( (dataPoint, chartIdx) => {
+                if (idx !== 0){
+                    if (combinedStats.chart[chartIdx].average === undefined) combinedStats.chart[chartIdx].average = 0;
+
+                    let value = dataPoint.average;
+                    if (value === null){
+                        value = stocks[symbol].chart[stocks[symbol].chart.length - 1].average;
+                    }
+                    combinedStats.chart[chartIdx].average += value;
                 }
-                combinedStats[dataPoint.label] += value;
             });
+
         });
         console.log(combinedStats);
-
-
+        combinedStats['price'] = user.portfolioValue;
+        let combinedChart = combinedStats['chart'];
+        let last = combinedChart[combinedChart.length - 1];
+        let first = combinedChart[0];
+        debugger
+        combinedStats['percentageChange'] = ((last.average / first.average - 1 ) * 100).toFixed(2);
+        combinedStats['dollarChange'] = (last.average - first.average).toFixed(2);
 
         return (
             <>
