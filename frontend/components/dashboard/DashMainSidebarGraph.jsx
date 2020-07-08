11cac667ccd360da047b6dd64b2906f2d8f0db1c
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-08 11:52:24 -0400

    Add previous close to all graphs except portfolio, and remove static bottom border

--- frontend/components/dashboard/DashMainSidebarGraph.jsx
+++ frontend/components/dashboard/DashMainSidebarGraph.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts';
 import {useSelector} from 'react-redux'
 
 const DashMainSidebarGraph = ({data, symbol}) => {
@@ -11,7 +11,8 @@ const DashMainSidebarGraph = ({data, symbol}) => {
             <>
                 <ResponsiveContainer>
                     <LineChart data={data}>
-                        <Line isAnimationActive={false} connectNulls={true} type="linear" dataKey={"average"} stroke={strokeColor} dot={false} strokeWidth="2" />
+                        <Line isAnimationActive={false} connectNulls={true} type="linear" dataKey={"average"} stroke={strokeColor} dot={false} strokeWidth=".5" />
+                        <ReferenceLine y={data[0].open} stroke="rgb(184, 181, 181)" strokeDasharray="3 3" />
                         <XAxis hide={true} dataKey="label" />
                         <YAxis domain={['auto', 'auto']} hide={true} />
                     </LineChart>
