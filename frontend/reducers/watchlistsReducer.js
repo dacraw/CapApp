13c48fa6ad1775d2ac9e4742bcc6d21fe58d9934
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-08 11:14:21 -0400

    Create watchlist reducer

--- frontend/reducers/watchlistsReducer.js
+++ frontend/reducers/watchlistsReducer.js
@@ -1,2 +1,20 @@
 import {RECEIVE_ALL_WATCHLISTS, RECEIVE_SINGLE_WATCHLIST} from '../actions/watchlistActions'
+import _ from 'lodash'
 
+const _null = [{
+    id: "",
+    title: "",
+    stocks: [],
+}];
+
+export default (state = _null, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_ALL_WATCHLISTS:
+            return _.merge({}, state, action.watchlists)
+        case RECEIVE_SINGLE_WATCHLIST:
+            return _.merge({}, state, action.watchlist)         
+        default:
+            return state
+    }
+}
