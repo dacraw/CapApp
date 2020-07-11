6ea32f0f23b31f34746d9d5b55255f861e757112
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-11 17:16:20 -0400

    Add watchlist sidebar controls to dashboard

--- frontend/components/dashboard/SidebarWatchlist.jsx
+++ frontend/components/dashboard/SidebarWatchlist.jsx
@@ -35,10 +35,15 @@ export default (props) => {
             </Route>
             <Route path="/">
             <>
-                <Link className="watchlist" to={`watchlist/${watchlist.id}`}>
-                    <i class="fas fa-lightbulb"></i>
+            <Link className="watchlist" to={`watchlist/${watchlist.id}`}>
+                    <i className="fas fa-lightbulb"></i>
                     <h2 className="watchlist-title">{watchlist.title}</h2>
-                    
+                    <i onClick={showOptions} key={watchlist.id} data-key={watchlist.id} className="fas fa-ellipsis-h">
+                        <div className={`watchlist-sidebar-options watchlist-options-${watchlist.id}`}>
+                            <Link to={`watchlist/${watchlist.id}`}>Edit List</Link>
+                            <button onClick={() => dispatch(deleteWatchlist(watchlist.id))} type="button">Delete List</button>
+                        </div>
+                    </i>
                 </Link>
             </>
             </Route>
