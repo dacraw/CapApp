3506c8708d134697feb4331c30803f8e253f8ca6
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-17 19:33:16 -0400

    Recaulcate portfolio value for history

--- frontend/components/dashboard/main.jsx
+++ frontend/components/dashboard/main.jsx
@@ -32,8 +32,10 @@ class Dashboard extends React.Component {
         ownedStockSymbols.forEach( (symbol, idx) => {
             if (idx == 0) {
                 combinedStats['chart'] = _.cloneDeep(stocks[symbol].chart);
+                combinedStats['chart'].forEach(chartItem => chartItem.average = 0)
             }
             stocks[symbol].chart.forEach( (dataPoint, chartIdx) => {
+                // if (chartIdx == 0) stocks[symbol].chart.average = 0
                 // if (idx !== 0){
                     let year = dataPoint.date.slice(0,4);
                     let month = parseInt(dataPoint.date.slice(5,7)) - 1;
