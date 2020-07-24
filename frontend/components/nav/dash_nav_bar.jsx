ab41cbfd38333ddec48f5c35a6d8a7e45947ac91
douglas_crawford <doug.a.crawford@gmail.com>
2020-07-24 15:36:12 -0400

    Change splash footer info. Add personal links to dashboard nav

--- frontend/components/nav/dash_nav_bar.jsx
+++ frontend/components/nav/dash_nav_bar.jsx
@@ -161,6 +161,9 @@ class DashNavBar extends React.Component{
                         <i className="fas fa-search"></i>
                     </div>
                     <ul className="links">
+                        <li><a href='https://dacraw.github.io/' target="_blank">Personal Site</a></li>
+                        <li><a href='https://github.com/dacrawford89' target="_blank">Github</a></li>
+                        <li><a href='http://www.linkedin.com/in/doug-a-crawford' target="_blank">LinkedIn</a></li>
                         <li><Link to="/dashboard">Portfolio</Link></li>
                         <li className="account-dropdown"><a onClick={this.toggleAccountDropdown}>Account</a>
                             <ul className="account-options" id="dash-nav-account-options">
