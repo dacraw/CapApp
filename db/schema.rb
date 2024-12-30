4e4709bc4dd0844c8bcf912fe353f92e59e26398
Doug <doug.a.crawford@gmail.com>
2024-12-30 00:11:15 -0800

    update to rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_07_220629) do
-
+ActiveRecord::Schema[7.2].define(version: 2024_12_30_064356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "daily_stock_quotes", force: :cascade do |t|
     t.bigint "stock_id"
     t.json "data"
-    t.datetime "date_start"
-    t.datetime "date_end"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "date_start", precision: nil
+    t.datetime "date_end", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["stock_id"], name: "index_daily_stock_quotes_on_stock_id"
   end
 
@@ -29,8 +28,8 @@ ActiveRecord::Schema.define(version: 2024_10_07_220629) do
     t.integer "user_id", null: false
     t.string "symbol", null: false
     t.float "num_shares", null: false
-    t.datetime "created_at", default: -> { "now()" }, null: false
-    t.datetime "updated_at", default: -> { "now()" }, null: false
+    t.datetime "created_at", precision: nil, default: -> { "now()" }, null: false
+    t.datetime "updated_at", precision: nil, default: -> { "now()" }, null: false
     t.float "stock_price"
     t.index ["created_at"], name: "index_portfolios_on_created_at"
     t.index ["num_shares"], name: "index_portfolios_on_num_shares"
@@ -40,8 +39,8 @@ ActiveRecord::Schema.define(version: 2024_10_07_220629) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "symbol", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "company"
     t.index ["symbol"], name: "index_stocks_on_symbol", unique: true
   end
@@ -50,8 +49,8 @@ ActiveRecord::Schema.define(version: 2024_10_07_220629) do
     t.string "username", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "fname", null: false
     t.string "lname", null: false
     t.float "cash_available"
@@ -62,17 +61,16 @@ ActiveRecord::Schema.define(version: 2024_10_07_220629) do
   create_table "watched_stocks", force: :cascade do |t|
     t.integer "watchlist_id", null: false
     t.integer "stock_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["watchlist_id", "stock_id"], name: "index_watched_stocks_on_watchlist_id_and_stock_id", unique: true
   end
 
   create_table "watchlists", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id", "title"], name: "index_watchlists_on_user_id_and_title", unique: true
   end
-
 end
