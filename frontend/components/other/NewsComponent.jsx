2d705a48ff4bccce0979e6cc8f5d715b6ecdac74
Doug <doug.a.crawford@gmail.com>
2024-12-31 23:18:12 -0800

    Add loading skeleton for news component on portfolios index

--- frontend/components/other/NewsComponent.jsx
+++ frontend/components/other/NewsComponent.jsx
@@ -1,24 +1,30 @@
-import {useSelector} from 'react-redux'
-import React, {useEffect} from 'react'
-import NewsComponentItem from './NewsComponentItem'
-import {useDispatch} from 'react-redux'
-import {fetchBusinessNews} from '../../actions/newsActions'
+import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import NewsComponentItem from "./NewsComponentItem";
+import { useDispatch } from "react-redux";
+import { fetchBusinessNews } from "../../actions/newsActions";
+import NewsComponentSkeleton from "./newsComponentSkeleton";
 
+export default function NewsComponent(props) {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.session.id);
+  const newsLoading = useSelector((state) => state.loading.newsLoader);
 
-export default (props) => {
-    const dispatch = useDispatch();
-    const currentUser = useSelector(state => state.session.id);
-    useEffect( () => {
-        dispatch(fetchBusinessNews())
-    }, []);
-    const news = useSelector(state => state.entities.news);
-    if (!currentUser || !Object.keys(news).length) return null
-    return (
-        <>
-            <div className="dashboard-news">
-                <h1>Today's Top Business News</h1>
-                {Object.values(news).map((newsItem, idx) => <NewsComponentItem key={idx} newsItem={newsItem} />)}
-            </div>
-        </>
-    )
-};
+  useEffect(() => {
+    dispatch(fetchBusinessNews());
+  }, []);
+
+  const news = useSelector((state) => state.entities.news);
+  return (
+    <div className="dashboard-news">
+      <h1>Today's Top Business News</h1>
+      {!currentUser || !Object.keys(news).length || newsLoading ? (
+        <NewsComponentSkeleton />
+      ) : (
+        Object.values(news).map((newsItem, idx) => (
+          <NewsComponentItem key={idx} newsItem={newsItem} />
+        ))
+      )}
+    </div>
+  );
+}
